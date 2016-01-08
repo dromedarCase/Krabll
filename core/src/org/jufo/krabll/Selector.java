0135f68f4146be566ab55548647057e5e4aafff1
Roman KR8 <kreiseltyp@hotmail.de>
2016-01-08 21:10:44 +0100

    added class structure

---
+++ core/src/org/jufo/krabll/Selector.java
@@ -0,0 +1,6 @@
+package org.jufo.krabll;
+
+
+public class Selector {
+    
+}
--- desktop/src/org/jufo/krabll/desktop/DesktopLauncher.java
+++ desktop/src/org/jufo/krabll/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package org.jufo.krabll.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import org.jufo.krabll.Krabll;
+import org.jufo.krabll.Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Krabll(), config);
+		new LwjglApplication(new Main(), config);
 	}
 }
