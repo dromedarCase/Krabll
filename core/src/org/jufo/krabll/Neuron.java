0bca836b9f6e0f884c9a7c13a769b05dd7e5f4b4
kreiseltyp <kreiseltyp@users.noreply.github.com>
2016-01-11 14:17:53 +0100

    updated Neuron.java

--- core/src/org/jufo/krabll/Neuron.java
+++ core/src/org/jufo/krabll/Neuron.java
@@ -3,8 +3,12 @@ package org.jufo.krabll;
 
 public class Neuron {
     int threshold;
+    int[] connectedInputs;
+    int[] connectedOutputs;
     
-    public Neuron(int thres){
+    public Neuron(int thres, int[] con, int[] conOut){
         threshold = thres;
+        connectedInputs = conIn;
+        connectedOutputs = conOut;
     }
 }
