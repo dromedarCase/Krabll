88bb78ca91f903273c02f65ce835addab3da0bce
dromedarCase <dromedarCase@sdf.org>
2016-01-16 14:17:31 +0100

    Box2D compatibilization

--- core/src/org/jufo/krabll/RigidBody.java
+++ core/src/org/jufo/krabll/RigidBody.java
@@ -2,13 +2,29 @@ package org.jufo.krabll;
 
 
 public class RigidBody {
-    int xCoordinate;
-    int yCoordinate;
+    //position on the plane
+    double xCoordinate;
+    double yCoordinate;
     short rotation;
     
-    public RigidBody(int xCoordinate, int yCoordinate, short rotation){
+    //movement and collision handling
+    int mass;
+    double speed;
+    double length;
+    
+    public RigidBody(double xCoordinate, double yCoordinate, short rotation){
         this.xCoordinate = xCoordinate;
         this.yCoordinate = yCoordinate;
         this.rotation = rotation;
+        this.mass = 0;
+        this.speed = 0;
+    }
+    
+    public RigidBody(double xCoordinate, double yCoordinate, short rotation, int mass){
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.rotation = rotation;
+        this.mass = mass;
+        this.speed = 0;
     }
 }
