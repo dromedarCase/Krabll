46115a3a246c40bd5b43ddac86efb1d810b4d1a0
Roman KR8 <kreiseltyp@users.noreply.github.com>
2016-01-17 14:27:37 +0100

    changed some comments and name from Roman KR8 to kreiseltyp

--- core/src/org/jufo/krabll/Krabll.java
+++ core/src/org/jufo/krabll/Krabll.java
@@ -11,11 +11,13 @@ public class Krabll extends RigidBody {
     }
     
     // TODO: recognizing inputs
+    // applies inputs
     public void applyInputs(){
         
     }
     
     // TODO: performing outputs
+    // performs outputs
     public void performOutputs(){
         
     }
